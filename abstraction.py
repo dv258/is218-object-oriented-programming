906b43d4c592bc391a156c8e08d98e8342b49624
dv258 <60153503+dv258@users.noreply.github.com>
2020-02-21 23:43:12 -0500

    add comments

--- abstraction.py
+++ abstraction.py
@@ -1,25 +1,33 @@
 class Library:
 	def __init__(self, books):
+		# store books in an array
 		self.books = []
 		for b in books:
 			self.books.append(b)
 
+		# a cache of books, where the map's keys are the book titles
 		self.__bookLookupTitleCache = {}
 
 	def findBookByTitle(self, title):
+		#the caller does not know about the internal storing or caching mechanism
+
+		#if the book is cached, just return the cached value
 		if title in self.__bookLookupTitleCache:
 			return self.__bookLookupTitleCache[title]
 
 		foundBook = None
 
+		#look for the book in the list of books
 		for b in self.books:
 			if b.title == title:
 				foundBook = b
 				break
 
+		#book not found
 		if foundBook is None:
 			return None
 
+		#add book to cache
 		self.__bookLookupTitleCache[foundBook.title] = foundBook
 		return foundBook
 
